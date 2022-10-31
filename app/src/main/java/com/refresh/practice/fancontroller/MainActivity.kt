abe78888255a180f25f3ec697e075eaa1a6fabd1
AliJonMadaminov <aliufc2004@gmail.com>
2022-10-31 13:18:06 +0500

    drawing DialView.kt implemented
    -but not interactive yet

---
+++ app/src/main/java/com/refresh/practice/fancontroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.refresh.practice.fancontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
